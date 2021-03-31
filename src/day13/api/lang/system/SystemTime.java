5d53eafe1e49670a47702334623f46e60fd6e441
ssong6735 <because6735@gmail.com>
2021-03-31 16:10:44 +0900

    [21.03.31] # API - java.lang.system

---
+++ src/day13/api/lang/system/SystemTime.java
@@ -0,0 +1,22 @@
+package day13.api.lang.system;
+
+public class SystemTime {
+
+    public static void main(String[] args) {
+        
+        long now = System.currentTimeMillis();
+        System.out.println("now = " + now);
+
+        // 알고리즘 시간복잡도 계산
+        long start = System.currentTimeMillis();
+        int total = 0;
+        for (int i = 0; i < 1000000000; i++) {
+            total += i;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("실행시간: " + (end - start) + "ms");
+
+        
+    }
+    
+}
