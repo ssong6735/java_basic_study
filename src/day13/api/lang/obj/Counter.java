badcf86576d5b851e09c9761e1d45851b1c71d6c
ssong6735 <because6735@gmail.com>
2021-03-31 16:10:35 +0900

    [21.03.31] # API - java.lang.obj

---
+++ src/day13/api/lang/obj/Counter.java
@@ -0,0 +1,43 @@
+package day13.api.lang.obj;
+
+public class Counter {
+
+    private int no;
+
+    // 생성자
+    public Counter(int no) {
+        this.no = no;
+    }
+
+    /*
+    // C++ 소멸자
+    ~Counter() { }
+    */
+
+    /*
+    // 파이썬 생성자
+    def __init__(self):
+    // 파이썬 소멸자
+    def __del__(self):
+    */
+
+
+    // 소멸자: 객체가 메모리에서 제거되는 시점에 자동으로 호출됨
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println(no + "번 객체 소멸됨");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("소멸자 테스트!");
+        Counter c = null;
+        for (int i = 0; i < 50; i++) {
+            c = new Counter(i);
+            c = null;
+            System.gc(); // 가비지 컬렉터를 빨리 호출
+        }
+
+    }
+
+}
---
+++ src/day13/api/lang/obj/Main.java
@@ -0,0 +1,12 @@
+package day13.api.lang.obj;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Member member = new Member("김철수", 30, "대전 서구");
+        System.out.println(member.toString());
+
+    }
+
+}
